c971e377c56d143a166657364b9428ca2c276042
kitmor@gmail.com <kitmor@gmail.com@a59f3039-f2c6-8282-4e09-36716e3c124a>
2012-06-27 07:19:57 +0000

    Started implmenting the medulla_lib library.

---
+++ trunk/firmware/medulla_lib/include/ethercat.h
@@ -0,0 +1,22 @@
+#ifndef ETHERCAT_H
+#define ETHERCAT_H
+
+/** \brief Driver for using the EtherCAT slave chip on the medulla board
+ *  
+ *  The ET1100 EtherCAT slave chip on the medulla board interfaces with the
+ *  medulla over an SPI interface. This driver handles the SPI communication
+ *  with the slave. It also handles mapping memory locations in xMega memory to
+ *  the memory locations of PDOs on the EtherCAT slave chip.
+ */
+
+#include <avr/io.h>
+#include "io_pin.h"
+
+typedef struct {
+	PORT_t *port_reg;
+	SPI_t *spi_reg;
+	io_pin_t eeprom_pin;
+	io_pin_t irq_pin
+} ec_HWConfig_t
+
+#endif // ETHERCAT_H
