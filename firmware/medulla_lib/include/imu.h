4ef80783d79548557f83016b831a34f07ec304a0
kitmor@gmail.com <kitmor@gmail.com@a59f3039-f2c6-8282-4e09-36716e3c124a>
2012-09-11 21:53:16 +0000

    Fixed IMU driver. You really need to use include protection.

--- firmware/medulla_lib/include/imu.h
+++ firmware/medulla_lib/include/imu.h
@@ -1,3 +1,9 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include <stdbool.h>
+#include "uart.h"
+
 #define IMU_RAW_DATA_CMD 0xC1
 #define IMU_ORIENTATION_DATA_CMD 0xC8
 
@@ -8,11 +14,15 @@ typedef struct{
 	uint8_t tx_buffer[IMU_TX_BUFFER_LENGTH];
 	uint8_t rx_buffer[IMU_RX_BUFFER_LENGTH];
 	uart_port_t uart_port;
-	uint8_t current_data[IMU_RX_BUFFER_LENGTH];
+	uint8_t *imu_data_buffer;
 }IMU_data_t;
 
-void initIMU(PORT_t *port, USART_t *uart, IMU_data_t *imu_data);
+void init_IMU(PORT_t *port, USART_t *uart, IMU_data_t *imu_data, uint8_t *imu_data_buffer);
 
-void IMURequestOrientation(IMU_data_t * imu_data);
+void IMU_request_orientation(IMU_data_t * imu_data);
 
-void IMUReceiveOrientation(IMU_data_t * imu_data);
+bool IMU_received_orientation(IMU_data_t * imu_data);
+
+void IMU_process_orientation(IMU_data_t * imu_data);
+
+#endif //IMU_H
