5831752c01e87b8703c589d0cb4c2fb8f26f272d
kitmor@gmail.com <kitmor@gmail.com@a59f3039-f2c6-8282-4e09-36716e3c124a>
2012-09-11 21:53:16 +0000

    Fixed IMU driver. You really need to use include protection.

--- trunk/firmware/medulla_lib/src/imu.c
+++ trunk/firmware/medulla_lib/src/imu.c
@@ -1,28 +1,31 @@
 #include "uart.h"
 #include "imu.h"
-#include "avr_compiler.h"
 
 //Based upon Information from Single Byte Command Protocol Datasheet
 //for 3DM-GX3-25
 
-void initIMU(PORT_t *port, USART_t *uart, IMU_data_t *imu_data){
+void init_IMU(PORT_t *port, USART_t *uart, IMU_data_t *imu_data, uint8_t *imu_data_buffer){
 
 	//Same as regular UART, needs 460800 baud
 	imu_data->uart_port = uart_init_port(port, uart, 0xD01B, imu_data->tx_buffer, IMU_TX_BUFFER_LENGTH, imu_data->rx_buffer, IMU_RX_BUFFER_LENGTH);
 
-
 	uart_connect_port(&(imu_data->uart_port), 0);
+	imu_data->imu_data_buffer = imu_data_buffer;
 }
 
-void IMURequestOrientation(IMU_data_t * imu_data){
+void IMU_request_orientation(IMU_data_t * imu_data){
 	(imu_data->tx_buffer)[0] = IMU_ORIENTATION_DATA_CMD;
 	uart_tx_data(&(imu_data->uart_port), imu_data->tx_buffer, 1);
 }
 
-void IMUReceiveOrientation(IMU_data_t * imu_data){
-	uint8_t waiting_for = 67;
-	while(waiting_for){
-		waiting_for -= uart_rx_data(&(imu_data->uart_port), &(imu_data->current_data[67 - waiting_for]), waiting_for);
-		}
+bool IMU_received_orientation(IMU_data_t * imu_data){
+	return (uart_received_bytes(&(imu_data->uart_port)) >= 67);
+}
+
+void IMU_process_orientation(IMU_data_t * imu_data){
+	if (IMU_received_orientation(imu_data) == false)
+		return;
+
+	uart_rx_data(&(imu_data->uart_port),imu_data->imu_data_buffer,64);
 }
 
