778791269cca9d223501b8bae8c38d77232e31ba
kitmor@gmail.com <kitmor@gmail.com@a59f3039-f2c6-8282-4e09-36716e3c124a>
2013-05-06 22:49:06 +0000

    Implemented interrupt for ad7193 adc driver.

--- trunk/firmware/medulla_lib/include/ad7193.h
+++ trunk/firmware/medulla_lib/include/ad7193.h
@@ -2,31 +2,28 @@
 #define AD7193_ADC_H
 
 /** @file
- *  @brief Driver for controling the AD7193 serial ADC using the USART port on the xmega
+ *  @brief Driver for controlling the AD7193 SPI ADC using one of the usart ports 
+ *  on the xmega.
+ *
+ *  Driver for controlling the AD7193 SPI ADC using one of the usart ports on the 
+ *  xmega. Currently the driver only supports reading a single usart at a time since
+ *  this is the only way only way to achieve 1Khz update speed.
  */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
-#include "io_pin.h"
-
 typedef struct {
 	PORT_t *usart_port;
 	USART_t *usart_reg;
-	io_pin_t CS_pin;
-	uint16_t *ch0_destination;
-	uint16_t *ch1_destination;
-	uint16_t *ch2_destination;
-	uint16_t *ch3_destination;
+	uint16_t *destination;
 	volatile bool currently_reading;
 } ad7193_t;
 
 typedef struct {
-	uint8_t tx_buffer[8];
-	uint8_t rx_buffer[8];
-	volatile uint8_t tx_buffer_position;
-	volatile uint8_t rx_buffer_position;
+	uint8_t buffer[4];
+	volatile uint8_t buffer_position;
 	ad7193_t *adc_pntr;
 } _ad7193_buffer_t;
 
@@ -40,21 +37,14 @@ _ad7193_buffer_t _usart_adc_USARTC0,
                     _ad7193_USARTF1;
 
 #define AD7193_USES_PORT(USART_PORT) \
-ISR(USART_PORT##_TXC_vect) { \
-	if (_ad7193_##USART_PORT.tx_buffer_position < 7) { \
-		USART_PORT.DATA = _ad7193_##USART_PORT.tx_buffer[++(_ad7193_##USART_PORT.tx_buffer_position)]; \
-	} \
-} \
-\
 ISR(USART_PORT##_RXC_vect) { \
-	_ad7193_##USART_PORT.rx_buffer[_ad7193_##USART_PORT.rx_buffer_position++] = USART_PORT.DATA; \
-	if (_ad7193_##USART_PORT.rx_buffer_position > 7) { \
-		(_ad7193_##USART_PORT.adc_pntr)->currently_reading = false; \
-		_ad7193_##USART_PORT.adc_pntr->CS_pin.io_port->OUTSET = 1<<_ad7193_##USART_PORT.adc_pntr->CS_pin.pin; \
-	} \
-}\
+	if (_ad7193_##USART_PORT.buffer_position > 0)  \
+		_ad7193_##USART_PORT.buffer[5 - _ad7193_##USART_PORT.buffer_position] = USART_PORT.DATA; \
+	if (_ad7193_##USART_PORT.buffer_position < 4)  \
+		USART_PORT.DATA = 0; \
+} \
 
-ad7193_t ad7193_init(PORT_t *usart_port, USART_t *usart_reg, io_pin_t CS_pin, uint16_t *ch0_dest,uint16_t *ch1_dest,uint16_t *ch2_dest,uint16_t *ch3_dest);
+ad7193_t ad7193_init(PORT_t *usart_port, USART_t *usart_reg, uint16_t *destination);
 
 void ad7193_start_read(ad7193_t *usart_adc);
 
--- trunk/firmware/medulla_lib/src/ad7193.c
+++ trunk/firmware/medulla_lib/src/ad7193.c
@@ -1,105 +1,14 @@
-#include "adc124.h"
+#include "ad7193.h"
 
-adc124_t adc124_init(PORT_t *usart_port, USART_t *usart_reg, io_pin_t CS_pin, uint16_t *ch0_dest,uint16_t *ch1_dest,uint16_t *ch2_dest,uint16_t *ch3_dest) {
-	PORTC.DIRSET = 1;
-	// setup the adc struct
-	adc124_t adc;
-	adc.usart_port = usart_port;
-	adc.usart_reg = usart_reg;
-	adc.ch0_destination = ch0_dest;
-	adc.ch1_destination = ch1_dest;
-	adc.ch2_destination = ch2_dest;
-	adc.ch3_destination = ch3_dest;
-	adc.currently_reading = false;
-
-	usart_port->DIRSET = 1<<1 | 1<<3;
-
-	// now setup the usart for SPI mode
-	usart_reg->CTRLA = USART_TXCINTLVL_MED_gc | USART_RXCINTLVL_MED_gc;
-	usart_reg->CTRLB = USART_RXEN_bm | USART_TXEN_bm;
-	usart_reg->CTRLC = USART_CMODE_MSPI_gc;
-	usart_reg->BAUDCTRLA = 7;
-
-	// Setup the chip select pin
-	adc.CS_pin = CS_pin;
-	io_set_direction(CS_pin,io_output);
-	io_set_output(CS_pin,io_high);
-
-	// setup the transmit buffer
-	_adc124_buffer_t *buffer;
-
-	switch((intptr_t)usart_reg) {
-		case ((intptr_t)&USARTC0): buffer = &_adc124_USARTC0; break;
-		case ((intptr_t)&USARTC1): buffer = &_adc124_USARTC1; break;
-		case ((intptr_t)&USARTD0): buffer = &_adc124_USARTD0; break;
-		case ((intptr_t)&USARTD1): buffer = &_adc124_USARTD1; break;
-		case ((intptr_t)&USARTE0): buffer = &_adc124_USARTE0; break;
-		case ((intptr_t)&USARTE1): buffer = &_adc124_USARTE1; break;
-		case ((intptr_t)&USARTF0): buffer = &_adc124_USARTF0; break;
-		case ((intptr_t)&USARTF1): buffer = &_adc124_USARTF1; break;
-		default: buffer = 0;
-	}
-	buffer->tx_buffer[0] = 1<<3;
-	buffer->tx_buffer[1] = 0;
-	buffer->tx_buffer[2] = 2<<3;
-	buffer->tx_buffer[3] = 0;
-	buffer->tx_buffer[4] = 3<<3;
-	buffer->tx_buffer[5] = 0;
-	buffer->tx_buffer[6] = 0<<3;
-	buffer->tx_buffer[7] = 0;
-
-	return adc;
+ad7193_t adc7193_init(PORT_t *usart_port, USART_t *usart_reg, uint16_t *destination) {
 }
 
-void adc124_start_read(adc124_t *adc) {
-	if (adc->currently_reading) {
-		return;
-	}
-
-	adc->currently_reading = true;
-	_adc124_buffer_t *buffer;
-
-	switch((intptr_t)adc->usart_reg) {
-		case ((intptr_t)&USARTC0): buffer = &_adc124_USARTC0; break;
-		case ((intptr_t)&USARTC1): buffer = &_adc124_USARTC1; break;
-		case ((intptr_t)&USARTD0): buffer = &_adc124_USARTD0; break;
-		case ((intptr_t)&USARTD1): buffer = &_adc124_USARTD1; break;
-		case ((intptr_t)&USARTE0): buffer = &_adc124_USARTE0; break;
-		case ((intptr_t)&USARTE1): buffer = &_adc124_USARTE1; break;
-		case ((intptr_t)&USARTF0): buffer = &_adc124_USARTF0; break;
-		case ((intptr_t)&USARTF1): buffer = &_adc124_USARTF1; break;
-		default: buffer = 0;
-	}
-	io_set_output(adc->CS_pin,io_low);
-	buffer->adc_pntr = adc;
-
-	buffer->tx_buffer_position = 0;
-	buffer->rx_buffer_position = 0;
-	adc->usart_reg->DATA = buffer->tx_buffer[0];
+void ad7193_start_read(ad7193_t *adc) {
 }
 
-bool adc124_read_complete(adc124_t *adc) {
-	return !adc->currently_reading;
+bool ad7193_read_complete(ad7193_t *adc) {
 }
 
-void adc124_process_data(adc124_t *adc) {
-	_adc124_buffer_t *buffer;
-
-	switch((intptr_t)adc->usart_reg) {
-		case ((intptr_t)&USARTC0): buffer = &_adc124_USARTC0; break;
-		case ((intptr_t)&USARTC1): buffer = &_adc124_USARTC1; break;
-		case ((intptr_t)&USARTD0): buffer = &_adc124_USARTD0; break;
-		case ((intptr_t)&USARTD1): buffer = &_adc124_USARTD1; break;
-		case ((intptr_t)&USARTE0): buffer = &_adc124_USARTE0; break;
-		case ((intptr_t)&USARTE1): buffer = &_adc124_USARTE1; break;
-		case ((intptr_t)&USARTF0): buffer = &_adc124_USARTF0; break;
-		case ((intptr_t)&USARTF1): buffer = &_adc124_USARTF1; break;
-		default: buffer = 0;
-	}
-
-	*(adc->ch0_destination) = (((uint16_t)(buffer->rx_buffer[0]))<<8 | buffer->rx_buffer[1]) & 0xFFF;
-	*(adc->ch1_destination) = (((uint16_t)(buffer->rx_buffer[2]))<<8 | buffer->rx_buffer[3]) & 0xFFF;
-	*(adc->ch2_destination) = (((uint16_t)(buffer->rx_buffer[4]))<<8 | buffer->rx_buffer[5]) & 0xFFF;
-	*(adc->ch3_destination) = (((uint16_t)(buffer->rx_buffer[6]))<<8 | buffer->rx_buffer[7]) & 0xFFF;
+void ad7193_process_data(ad7193_t *adc) {
 }
 
