49b3a4c2958b3b7eaaf1d5c9b8215fb6c8bc2958
kitmor@gmail.com <kitmor@gmail.com@a59f3039-f2c6-8282-4e09-36716e3c124a>
2013-05-12 21:08:22 +0000

    Turns out that specification mixes word and byte addressing in it's tables, fixed sii_file.h header to correct for this.

--- software/sii_tool/sii_file.h
+++ software/sii_tool/sii_file.h
@@ -43,51 +43,51 @@
 #define CAT_TYPE_END                    0xFFFF      // This category type ID indicates the end of the category section
 
 // General Category Structure
-#define GENERAL_CAT_LENGTH              (19*2)
-#define GROUP_STR_INDEX_OFF         (0x0000*2)      // Offset of group string index in category (Index into strings)
-#define IMG_INDEX_OFF               (0x0001*2)
-#define ORDER_INDEX_OFF             (0x0002*2)
-#define NAME_INDEX_OFF              (0x0003*2)
-#define COE_DETAILS_OFF             (0x0005*2)
-#define FOE_DETAILS_OFF             (0x0006*2)
-#define EOE_DETAILS_OFF             (0x0007*2)
-#define FLAGS_OFF                   (0x000B*2)
-#define EBUS_CURRENT_OFF            (0x000C*2)
-#define PHY_PORT_OFF                (0x0010*2)
+#define GENERAL_CAT_LENGTH                (32)
+#define GROUP_STR_INDEX_OFF           (0x0000)      // Offset of group string index in category (Index into strings)
+#define IMG_INDEX_OFF                 (0x0001)
+#define ORDER_INDEX_OFF               (0x0002)
+#define NAME_INDEX_OFF                (0x0003)
+#define COE_DETAILS_OFF               (0x0005)
+#define FOE_DETAILS_OFF               (0x0006)
+#define EOE_DETAILS_OFF               (0x0007)
+#define FLAGS_OFF                     (0x000B)
+#define EBUS_CURRENT_OFF              (0x000C)
+#define PHY_PORT_OFF                  (0x0010)
 
 // Sync Manager Category structure, repeated for as many sync managers as used
-#define SYNCM_CAT_LEN                    (8*2)
-#define SYNCM_PHY_ADDR_OFF          (0x0000*2)
-#define SYNCM_LENGTH_OFF            (0x0002*2)
-#define SYNCM_CTRL_REG_OFF          (0x0004*2)
-#define SYNCM_ENABLE_OFF            (0x0006*2)
-#define SYNCN_TYPE_OFF              (0x0007*2)
+#define SYNCM_CAT_LEN                      (8)
+#define SYNCM_PHY_ADDR_OFF            (0x0000)
+#define SYNCM_LENGTH_OFF              (0x0002)
+#define SYNCM_CTRL_REG_OFF            (0x0004)
+#define SYNCM_ENABLE_OFF              (0x0006)
+#define SYNCN_TYPE_OFF                (0x0007)
 
 // PDO Category strucure, repeated for each PDO
-#define PDO_CAT_FIXED_LEN                (8*2)      // Length of fixed section of PDO category in addition to size of entry list
-#define PDO_INDEX_OFF               (0x0000*2)      // Offset of PDO index from beginning of PDO
-#define PDO_NENTRY_OFF              (0x0002*2)      // Offset of stored value of entries
-#define PDO_SYNCM_OFF               (0x0003*2)
-#define PDO_SYNCRONIZATION          (0x0004*2)
-#define PDO_NAME_INDEX_OFF          (0x0005*2)      // Index into strings category
-#define PDO_ENTRY_START             (0x0008*2)      // Offset of begging of PDO entry list
+#define PDO_CAT_FIXED_LEN                  (8)      // Length of fixed section of PDO category in addition to size of entry list
+#define PDO_INDEX_OFF                 (0x0000)      // Offset of PDO index from beginning of PDO
+#define PDO_NENTRY_OFF                (0x0002)      // Offset of stored value of entries
+#define PDO_SYNCM_OFF                 (0x0003)
+#define PDO_SYNCRONIZATION            (0x0004)
+#define PDO_NAME_INDEX_OFF            (0x0005)      // Index into strings category
+#define PDO_ENTRY_START               (0x0008)      // Offset of begging of PDO entry list
 
 // PDO Entry subcategory structure, repeated for each PDO entry
-#define ENTRY_CAT_LENGTH                 (8*2)
-#define ENTRY_INDEX_OFF             (0x0000*2)
-#define ENTRY_SUBINDEX_OFF          (0x0002*2)
-#define ENTRY_NAME_IDX_OFF          (0x0003*2)      // Index into strings category
-#define ENTRY_DATA_TYPE_OFF         (0x0004*2)
-#define ENTRY_BITLEN_OFF            (0x0005*2)
+#define ENTRY_CAT_LENGTH                   (8)
+#define ENTRY_INDEX_OFF               (0x0000)
+#define ENTRY_SUBINDEX_OFF            (0x0002)
+#define ENTRY_NAME_IDX_OFF            (0x0003)      // Index into strings category
+#define ENTRY_DATA_TYPE_OFF           (0x0004)
+#define ENTRY_BITLEN_OFF              (0x0005)
 
 // DC Clock Category structure
-#define DC_CAT_LENGTH                   (24*2)
-#define DC_CYCLE_SYNC0_OFF          (0x0000*2)
-#define DC_SHIFT_SYNC0_OFF          (0x0004*2)
-#define DC_SHIFT_SYNC1_OFF          (0x0008*2)
-#define DC_CYCLE_SYNC1_FACTOR_OFF   (0x000C*2)
-#define DC_ASSIGN_ACTIVATE_OFF      (0x000E*2)
-#define DC_CYCLE_SYNC0_FACTOR_OFF   (0x0010*2)
-#define DC_SYNC_NAME_INDEX_OFF      (0x0012*2)      // Index into strings category
+#define DC_CAT_LENGTH                     (24)
+#define DC_CYCLE_SYNC0_OFF            (0x0000)
+#define DC_SHIFT_SYNC0_OFF            (0x0004)
+#define DC_SHIFT_SYNC1_OFF            (0x0008)
+#define DC_CYCLE_SYNC1_FACTOR_OFF     (0x000C)
+#define DC_ASSIGN_ACTIVATE_OFF        (0x000E)
+#define DC_CYCLE_SYNC0_FACTOR_OFF     (0x0010)
+#define DC_SYNC_NAME_INDEX_OFF        (0x0012)      // Index into strings category
 
 #endif // SII_FILE_H
