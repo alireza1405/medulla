05155ff12036c87d36fd468c11f4732d01f2e8f9
Ryan.Domres <Ryan.Domres@a59f3039-f2c6-8282-4e09-36716e3c124a>
2015-01-15 17:13:11 +0000

    Comparator driver updated to use voltage scaler.

--- firmware/medulla_lib/src/ac.c
+++ firmware/medulla_lib/src/ac.c
@@ -1,24 +1,26 @@
 #include "ac.h"
 
-ac_port_t ac_init_port(AC_t *ac) {
+ac_port_t ac_init_port(AC_t *ac, uint8_t therm_max) {
 	// Setup the port struct
 	ac_port_t port;
 	port.ac_port = ac;
 	port.pin_0 = 0;
 	port.pin_1 = 0;
+	// Clean input
+	therm_max &= 0x3F;
 
 	// Setup control registers
 	ac->CTRLA = 0 << AC_AC0OUT_bp; // Disable pin 7 output of comparator 0
-	ac->CTRLB = AC_SCALEFAC_gm; // Set scaling factor to 1
-	// Disable interrupt, set hysteresis mode to large, and enable comparator
-	ac->AC0CTRL = (AC_INTLVL_OFF_gc | AC_HYSMODE_SMALL_gc | AC_ENABLE_bm);
-	ac->AC1CTRL = (AC_INTLVL_OFF_gc | AC_HYSMODE_SMALL_gc | AC_ENABLE_bm);
+	ac->CTRLB = therm_max << AC_SCALEFAC_gp; // Set scaling factor
+	// Disable interrupt, set hysteresis mode to none, and enable comparator
+	ac->AC0CTRL = (AC_INTLVL_OFF_gc | AC_HYSMODE_NO_gc | AC_ENABLE_bm);
+	ac->AC1CTRL = (AC_INTLVL_OFF_gc | AC_HYSMODE_NO_gc | AC_ENABLE_bm);
 	// Disable window function and interrupt
 	ac->WINCTRL &= ~(AC_WEN_bm | AC_WINTLVL_gm);
 
-	// Set the negative output of both comparators to the DAC output.
-	ac->AC0MUXCTRL = AC_MUXNEG_DAC_gc;
-	ac->AC1MUXCTRL = AC_MUXNEG_DAC_gc;
+	// Set the negative output of both comparators to the scaler output.
+	ac->AC0MUXCTRL = AC_MUXNEG_SCALER_gc;
+	ac->AC1MUXCTRL = AC_MUXNEG_SCALER_gc;
 
 	return port;
 }
@@ -35,12 +37,10 @@ void ac_set_pins(ac_port_t *ac, uint8_t pin_0, uint8_t pin_1) {
 }
 
 bool ac_check_value(ac_port_t *ac, uint8_t pin) {
-	/*if (pin && (ac->ac_port->STATUS & AC_AC1STATE_bm))
-		return 1;
+	if (pin && (ac->ac_port->STATUS & AC_AC1STATE_bm))
+		return 0;
 	if (!pin && (ac->ac_port->STATUS & AC_AC0STATE_bm))
-		return 1;
-		*/
-	if (ac->ac_port->STATUS & (AC_AC1STATE_bm | AC_AC0STATE_bm))
-		return 1;
-	return 0;
+		return 0;
+	// Input value is below negative input, return 1
+	return 1;
 }
